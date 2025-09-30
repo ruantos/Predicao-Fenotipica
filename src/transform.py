d83694e9c770d19d9801831306cecffb2ffa3226
Juan Oliveira <ruantos@proton.me>
2025-09-30 15:29:40 -0300

    fix: remove more unused columns

--- src/transform.py
+++ src/transform.py
@@ -7,7 +7,7 @@ COLS_TO_DROP = {
 		'Nome completo do voluntário: ', 'Idade na admissão: ',
 		'Reside em qual cidade: ', 'Grau de instrução: ',
 		'  Estado civil:   ', '  Profissão:  ', 'Sexo (gênero):',
-		'Data de nascimento: ', 'Cor da pele: '
+		'Data de nascimento: ', 'Tipo de Pele (Classificação de Fitzpatrick):  '
 		],
 	'genotypes':
 		[
@@ -33,12 +33,16 @@ def drop_columns(df: pd.DataFrame, sheet: int = 1) -> pd.DataFrame:
 def join_dataframes(df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
 	return df_1.join(df_2, how='outer')
 
+def reduce_col_sex(df: pd.DataFrame) -> pd.DataFrame:
+	df['sexo'] = df['sexo'].str[0]
+	return df
+
 
 def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
 	return df.rename(columns={
 		'Número de identificação (Iniciar com EC, acrescido de três dígitos - ex: EC001): ': 'id_voluntario',
 		'Data de nascimento: ': 'data_nascimento',
-		'Tipo de Pele (Classificação de Fitzpatrick):  ': 'tipo_pele',
+		'Cor da pele: ': 'cor_pele',
 		'  Cor dos olhos:  ': 'cor_olhos',
 		'  Cor dos cabelos quando tinha 15 anos:  ': 'cor_cabelo_15_anos'
 	})
@@ -54,6 +58,7 @@ def transform(dfs: list[pd.DataFrame]) -> pd.DataFrame:
 	df = join_dataframes(df_1, df_2)
 	df = concatenate_dfs(df, df_3)
 	df = rename_columns(df)
+	df = reduce_col_sex(df)
 
 
 	return df
